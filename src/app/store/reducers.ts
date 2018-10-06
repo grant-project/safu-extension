48ce6b7e57fd09964539209258eb76b2421d545a
Aaron <aaron.m.stricker@gmail.com>
2018-10-06 13:39:22 -0700

    rem reac-router-redux refs

--- src/app/store/reducers.ts
+++ src/app/store/reducers.ts
@@ -1,11 +1,9 @@
 // tslint:disable no-empty-interface
 import { combineReducers } from 'redux';
-import { routerReducer as routing, RouterState } from 'react-router-redux'
 import crypto, { CryptoState, INITIAL_STATE as cryptoInitialState } from 'modules/crypto';
 
 export interface AppState {
   crypto: CryptoState;
-  routing: RouterState;
 }
 
 export const combineInitialState: Partial<AppState> = {
@@ -14,5 +12,4 @@ export const combineInitialState: Partial<AppState> = {
 
 export default combineReducers<AppState>({
   crypto,
-  routing,
 });
